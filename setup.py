1ea1f113c0205555ca492a7ca0d83bfb72c5949a
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2016-10-11 09:50:34 +0200

    setup stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="tangods-eigerdectris_filewriter",
+      version="0.1.0",
+      description="Filewriter Device server for the Eiger detector",
+      py_modules=['EigerFilewriter'],
+      package_dir={'': 'dectris_eiger'},
+      )
